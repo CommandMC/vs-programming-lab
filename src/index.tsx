31681a191cb53c2c45b4ec23c778443de14241c0
Mathis Dröge <mathis.droege@ewe.net>
2025-02-26 09:22:24 +0100

    Give React Router a fallback element
    This is rendered while the main UI is loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import { Box, CircularProgress } from '@mui/material'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -16,6 +17,18 @@ const router = createBrowserRouter([
       const { default: App } = await import('./app')
       return { Component: App }
     },
+    hydrateFallbackElement: (
+      <Box
+        sx={{
+          display: 'flex',
+          height: '100vh',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    ),
     children: [
       {
         index: true,
