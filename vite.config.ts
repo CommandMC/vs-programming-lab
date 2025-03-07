9962c6801ea7ad6185d7406b832097ad4628968f
Mathis Dröge <mathis.droege@ewe.net>
2025-03-07 10:50:51 +0100

    Copy Leaflet assets to out dir
    This makes the marker & layer images work in the production build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server: {
     open: true
   },
-  plugins: [react(), svgr()]
+  plugins: [
+    react(),
+    svgr(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/leaflet/dist/images/*',
+          dest: ''
+        }
+      ]
+    })
+  ]
 })
